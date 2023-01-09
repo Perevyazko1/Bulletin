11566c04cc46dcf474537d8abd289481d919fcbd
Perevyazko <a.perevyazko@gmail.com>
2023-01-09 15:06:33 +0300

    debug flake8 --select I202

--- project/bulletin/templatetags/custom_tags.py
+++ project/bulletin/templatetags/custom_tags.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from django.utils import timezone
-
 from django import template
 
 register = template.Library()
--- project/project/urls.py
+++ project/project/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
-
 from accounts.views import authenticate
 from bulletin.views import send_mail
 
